20c1d22f966a37f2ab7424ca6bbe13e9cff36b54
kimminje0ng <kimmin9642@dgu.ac.kr>
2022-12-05 19:19:00 +0900

    Modify main.py for Hash Algorithm

--- Main.cpp
+++ Main.cpp
@@ -2,20 +2,28 @@
 #include "Core/Course.h"
 #include "Module/BccCleaning/TimeTable3.h"
 #include "Module/BruteForce/TimeTable2.h"
+#include "Module/Hash/TimeTable1.h"
 
-void timeTable2(CourseTable& course_table) {
+void Test_TimeTable1(CourseTable& course_table)
+{
+	TimeTable1 timetable1;
+	timetable1.generator(course_table);
+}
+
+void Test_TimeTable2(CourseTable& course_table) {
 	TimeTable2 table;
 	table.generator(course_table);
 }
+
 
 int main()
 {
 	CourseTable course_table;
 
-	// 1.
+	// 1. Hash
+	Test_TimeTable1(course_table);
 	// 2. Brute Force
-	timeTable2(course_table);
-
+	Test_TimeTable2(course_table);
 	// 3. 
 
 	return 0;
